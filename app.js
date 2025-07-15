a9194e7d95bcbe08e9aa26daf07c78020e6db43c
Kathula-Rajkumar <rajkumar@Rajkumars-MacBook-Air.local>
2025-07-15 23:48:40 +0530

    Initial commit - Wanderlust app

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const MONGO_URL ="mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL || "mongodb://127.0.0.1:27017/wanderlust";
+
 //const dbUrl = process.env.ATLASDB_URL;
 
 main()
@@ -34,7 +35,8 @@ main()
   });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+   await mongoose.connect(dbUrl);
+
   }
 
 app.set("view engine", "ejs");
